de2bdc714b1b836f0739dca28e9080a89f863e15
mario <marord_19@hotmail.com>
2019-11-01 15:54:44 -0600

    caso

--- front/pages/casos/_id.vue
+++ front/pages/casos/_id.vue
@@ -2,7 +2,14 @@
   <div>
 
     <h1 v-text="caso.titulo"></h1>
+
+    <p>DESCRIPCION DEL CASO</p>
     <p v-text="caso.cuerpo"></p>
+
+    <p>OFICINA</p>
+    <p v-text="oficinas.nombre"></p>
+
+
   </div>
 </template>
 
@@ -11,7 +18,8 @@
         name: "_id",
         data(){
             return{
-                caso: {}
+                caso: {},
+                oficinas: {}
             }
         },
         created(){
@@ -21,8 +29,11 @@
             async getDatos(){
                 try{
                     var res = await this.$axios.$get('api/casos/'+this.$route.params.id);
+                    var resOf = await this.$axios.$get('api/oficinas/'+this.$route.params.id);
+
 
                     this.caso = res.data;
+                    this.oficinas = resOf.data;
                     this.consolaJs(res);
                 }catch (e) {
                     this.consolaJs(e);
